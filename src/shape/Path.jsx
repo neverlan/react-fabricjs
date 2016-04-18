a06a5fd0d39e0aaa66676ddee82c790640cb3a1f
Xhriman <seal789ie@gmail.com>
2016-04-18 17:13:21 +0800

    path & pathgroup

---
+++ src/shape/Path.jsx
@@ -0,0 +1,65 @@
+'use strict';
+
+import {PropTypes} from 'react';
+import FabricObject from '../base/Object.jsx';
+import {fabric} from 'fabric-webpack';
+
+export default class Path extends FabricObject {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			object: null,
+		};
+
+		this.draw = this.draw.bind(this);
+
+		this.toString = () => this.state.object &&
+			this.state.object.toString();
+		this.toObject = (propertiesToInclude) => this.state.object &&
+			this.state.object.toObject(propertiesToInclude);
+		this.toDatalessObject = (propertiesToInclude) =>
+			this.state.object.toDatalessObject(propertiesToInclude);
+		this.toSVG = (reviver) => this.state.object &&
+			this.state.object.toSVG(reviver);
+		this.complexity = () => this.state.object &&
+			this.state.object.complexity();
+	}
+
+	draw(canvas) {
+		if (this.props.element instanceof Object) {
+			fabric.Path.fromElement(
+				this.props.element,
+				(instance) => super.draw(canvas, instance),
+				this.props
+			);
+		} else if (this.props.object instanceof Object) {
+			fabric.Path.fromObject(
+				this.props.object,
+				(instance) => super.draw(canvas, instance)
+			);
+		} else {
+			const object = new fabric.Path(this.props.path, this.props);
+			super.draw(canvas, object);
+
+			return object;
+		}
+	}
+
+}
+
+Path.fromElement = (element, callback, options) => fabric.Path.fromElement(element, callback, options);
+Path.fromObject = (object, callback) => fabric.Path.fromObject(object, callback);
+Path.attribute = fabric.Path.ATTRIBUTE_NAMES;
+
+Path.propTypes = Object.assign(FabricObject.propTypes, {
+	path: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+	minX: PropTypes.number,
+	minY: PropTypes.number,
+});
+
+Path.defaultProps = Object.assign(FabricObject.defaultProps, {
+	type: 'path',
+	path: null,
+	minX: 0,
+	minY: 0,
+});
---
+++ src/shape/PathGroup.jsx
@@ -0,0 +1,71 @@
+'use strict';
+
+import {PropTypes} from 'react';
+import Path from './Path';
+import {fabric} from 'fabric-webpack';
+
+export default class PathGroup extends Path {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			object: null,
+		};
+
+		this.draw = this.draw.bind(this);
+
+		this.parseDimensionsFromPaths = (options) => this.state.object &&
+			this.state.object.parseDimensionsFromPaths(options);
+		this.renderf = (ctx) => this.state.object &&
+			this.state.object.renderf(ctx);
+		this.toObject = (propertiesToInclude) =>
+			this.state.object.toObject(propertiesToInclude);
+		this.toDatalessObject = (propertiesToInclude) => this.state.object &&
+			this.state.object.toDatalessObject(propertiesToInclude);
+		this.toSVG = (reviver) => this.state.object &&
+			this.state.object.toSVG(reviver);
+		this.toString =  () => this.state.object &&
+			this.state.object.toSVG();
+		this.isSameColor =  () => this.state.object &&
+			this.state.object.isSameColor();
+		this.complexity =  () => this.state.object &&
+			this.state.object.complexity();
+		this.getObjects =  () => this.state.object &&
+			this.state.object.getObjects();
+
+	}
+
+	draw(canvas) {
+		if (this.props.element instanceof Object) {
+			fabric.PathGroup.fromElement(
+				this.props.element,
+				(instance) => super.draw(canvas, instance),
+				this.props
+			);
+		} else if (this.props.object instanceof Object) {
+			fabric.PathGroup.fromObject(
+				this.props.object,
+				(instance) => super.draw(canvas, instance)
+			);
+		} else {
+			const paths = [];
+			if (this.props.children instanceof Array) {
+				this.props.children.forEach(child => paths.push(new fabric.Path(child.props.path, child.props)));
+			} else {
+				paths.push(new fabric.Path(this.props.children.props.path, this.props.children.props));
+			}
+
+			const object = new fabric.PathGroup(paths, this.props);
+			super.draw(canvas, object);
+		}
+	}
+
+}
+
+PathGroup.fromElement = (element, callback, options) => fabric.PathGroup.fromElement(element, callback, options);
+PathGroup.fromObject = (object, callback) => fabric.PathGroup.fromObject(object, callback);
+PathGroup.attribute = fabric.PathGroup.ATTRIBUTE_NAMES;
+
+PathGroup.defaultProps = Object.assign(Path.defaultProps, {
+	type: 'path-group',
+	fill: '',
+});
